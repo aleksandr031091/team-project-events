17ecc8b559da206230eb8a078f9ce2ec0f30659c
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-07-09 01:51:50 +0300

    added search by id and also by keywords with country code

--- src/js/service/api.js
+++ src/js/service/api.js
@@ -1,22 +1,38 @@
 const API_KEY = 'iMT6lnWYsBtdSwl0jr9udmE86Jo5qsGK';
 
-const BASE_URL = `https://app.ticketmaster.com/discovery/v2/events.json`;
+const BASE_URL = `https://app.ticketmaster.com/discovery/v2/events`;
 
-const page = '';
+// const totalPage = 1;
 
-// const fetchEvents = async (BASE_URL, API_KEY) => {
-//   const url = `${BASE_URL}?size=10&apikey=${API_KEY}`;
-//   const events = await fetch(url);
-
-//   return events.json();
-// };
-
-// fetchEvents().then(data => console.log(data));
-
-function fetchEvents() {
-  const url = `${BASE_URL}?page=${page}0&apikey=${API_KEY}`;
-  return fetch(url).then(res => res.json());
+export const fetchEvents = async () => {
+  const url = `${BASE_URL}.json?&apikey=${API_KEY}`;
+  const API = await fetch(url);
   // .then(data => console.log(data));
-}
-fetchEvents().then(data => console.log(data));
-// export { fetchEvents };
+  return API.json();
+};
+
+export const fetchEventsBykeyword = async (keyword, countryCode) => {
+  const url = `${BASE_URL}.json?keyword=${keyword}&countryCode=${countryCode}&apikey=${API_KEY}`;
+  const API = await fetch(url);
+
+  // console.log(events);
+  return API.json();
+};
+
+export const fetchEventsById = async id => {
+  const url = `${BASE_URL}/${id}.json?&apikey=${API_KEY}`;
+  const API = await fetch(url);
+
+  // console.log(events);
+  return API.json();
+};
+
+// import { fetchEventsBykeyword, fetchEvents, fetchEventsById } from './service/api';
+// const keyword = 'Rock Fest';
+// const countryCode = 'US';
+// const id = 'G5v0ZpnSL0wtY';
+// // fetchEventsBykeyword(keyword, countryCode).then(res => console.log(res));
+
+// // fetchEvents().then(res => console.log(res));
+
+// fetchEventsById(id).then(res => console.log(res));
