39751ae827b2d110e78393b691c5fe2e579e5d2c
Afrikanochka <Mtgnatali@gmail.com>
2021-07-09 17:58:31 +0300

    add gallery, styles

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,18 +1,31 @@
-import { fetchEvents } from './service/api';
+// import { fetchEvents } from './service/api';
 import articlesTpl from '../templates/galleryTmp.hbs';
+import apiEvents from '../js/service/api';
 
 const refs = {
   gallery: document.querySelector('#gallery-js'),
 };
 
-fetchEvents().then(data => {
-  const events = data._embedded.events;
-  console.log(events);
-  const markUp = articlesTpl(events);
-  refs.gallery.innerHTML = markUp;
-  // console.log(events.images);
-});
+// fetchEvents().then(data => {
+//   const events = data._embedded.events;
+//   console.log(events);
+//   const markUp = articlesTpl(events);
+//   refs.gallery.innerHTML = markUp;
+//   // console.log(events.images);
+// });
 
 // .then(data => data.forEach(element => {
-
 // }))
+
+window.addEventListener('load', fetchEvents);
+
+function fetchEvents() {
+  apiEvents.fetchEvents().then(data => {
+    const events = data._embedded.events;
+    renderEvent(events);
+  });
+}
+
+function renderEvent(events) {
+  refs.gallery.insertAdjacentHTML('beforeend', articlesTpl(events));
+}
