7afd9257283134607240027e295d28bef0d4fc74
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-07-09 16:54:00 +0200

    render murcup dropdown menu

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,18 +1,18 @@
-import { fetchEvents } from './service/api';
-import articlesTpl from '../templates/galleryTmp.hbs';
+// import { fetchEvents } from './service/api';
+// import articlesTpl from '../templates/galleryTmp.hbs';
 
-const refs = {
-  gallery: document.querySelector('#gallery-js'),
-};
+// const refs = {
+//   gallery: document.querySelector('#gallery-js'),
+// };
 
-fetchEvents().then(data => {
-  const events = data._embedded.events;
-  console.log(events);
-  const markUp = articlesTpl(events);
-  refs.gallery.innerHTML = markUp;
-  // console.log(events.images);
-});
+// fetchEvents().then(data => {
+//   const events = data._embedded.events;
+//   console.log(events);
+//   const markUp = articlesTpl(events);
+//   refs.gallery.innerHTML = markUp;
+//   // console.log(events.images);
+// });
 
-// .then(data => data.forEach(element => {
+// // .then(data => data.forEach(element => {
 
-// }))
+// // }))
