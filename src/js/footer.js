b35382f7bcb0c8cb4cceaadca5debce8fd58cb57
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-07-11 17:31:36 +0300

    add logic for modal-footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,3 +1,19 @@
+import studentTpl from '../templates/students-modal.hbs';
+
+import * as basicLightbox from 'basiclightbox';
+import 'basicLightbox/dist/basicLightbox.min.css';
+
+const button = document.querySelector('#footer-button-js');
+
+console.log(button);
+
+button.addEventListener('click', onKlick);
+
+function onKlick(e) {
+  console.log(e.target);
+  basicLightbox.create(studentTpl()).show();
+}
+
 // (() => {
 //   const refs = {
 //     openModalBtn: document.querySelector('[data-modal-open]'),
