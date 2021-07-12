6e2078e16a073029a29f2814cd9af9e511b16b2c
serhiibulynin <serhiibulynin007@gmail.com>
2021-07-12 12:28:36 +0300

    added function in gallery

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -3,26 +3,51 @@ import Pagination from 'tui-pagination';
 import apiEvents from '../js/service/api';
 import renderGallery from './gallery';
 
-apiEvents.fetchEvents().then(events => {
+// apiEvents.fetchEvents().then(events => {
+//   const options = {
+//     totalItems: events.page.totalElements,
+//     itemsPerPage: events.page.size,
+//     visiblePages: 5,
+//     page: events.page.number + 1,
+//     centerAlign: false,
+//     firstItemClassName: 'tui-first-child',
+//     lastItemClassName: 'tui-last-child',
+//   };
+
+//   const pagination = new Pagination('pagination', options);
+
+//   pagination.on('afterMove', event => {
+//     apiEvents.page = event.page - 1;
+//     // apiEvents.page = 2000;
+//     apiEvents.fetchEvents().then(events => {
+//       console.log(events);
+//       renderGallery(events);
+//     });
+//   });
+//   renderGallery(events);
+// });
+
+function setPagination(totalItems) {
+  apiEvents.size = 20; //window.innerWidth
+  console.log(totalItems);
   const options = {
-    totalItems: events.page.totalElements,
-    itemsPerPage: events.page.size,
-    visiblePages: 5,
-    page: events.page.number + 1,
+    totalItems,
+    itemsPerPage: apiEvents.size, // window.innerWidth
+
+    visiblePages: 4, // window.innerWidth
+    page: 1,
     centerAlign: false,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
+    // firstItemClassName: 'tui-first-child',
+    // lastItemClassName: 'tui-last-child',
   };
 
   const pagination = new Pagination('pagination', options);
 
   pagination.on('afterMove', event => {
     apiEvents.page = event.page - 1;
-    // apiEvents.page = 2000;
     apiEvents.fetchEvents().then(events => {
       console.log(events);
       renderGallery(events);
     });
   });
-  renderGallery(events);
-});
+}
