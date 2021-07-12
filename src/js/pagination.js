2756b2fcbe4649af5e4c1719574805622d2d0fd6
serhiibulynin <serhiibulynin007@gmail.com>
2021-07-12 12:42:34 +0300

    added export function

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,26 +4,25 @@ import apiEvents from '../js/service/api';
 import renderGallery from './gallery';
 
 // apiEvents.fetchEvents().then(events => {
-//   const options = {
-//     totalItems: events.page.totalElements,
-//     itemsPerPage: events.page.size,
-//     visiblePages: 5,
-//     page: events.page.number + 1,
-//     centerAlign: false,
-//     firstItemClassName: 'tui-first-child',
-//     lastItemClassName: 'tui-last-child',
-//   };
+//   // const options = {
+//   //   totalItems: events.page.totalElements,
+//   //   itemsPerPage: events.page.size,
+//   //   visiblePages: 5,
+//   //   page: events.page.number + 1,
+//   //   centerAlign: false,
+//   //   firstItemClassName: 'tui-first-child',
+//   //   lastItemClassName: 'tui-last-child',
+//   // };
 
-//   const pagination = new Pagination('pagination', options);
+//   // const pagination = new Pagination('pagination', options);
 
-//   pagination.on('afterMove', event => {
-//     apiEvents.page = event.page - 1;
-//     // apiEvents.page = 2000;
-//     apiEvents.fetchEvents().then(events => {
-//       console.log(events);
-//       renderGallery(events);
-//     });
-//   });
+//   // pagination.on('afterMove', event => {
+//   //   apiEvents.page = event.page - 1;
+//   //   apiEvents.fetchEvents().then(events => {
+//   //     console.log(events);
+//   //     renderGallery(events);
+//   //   });
+//   // });
 //   renderGallery(events);
 // });
 
@@ -51,3 +50,5 @@ function setPagination(totalItems) {
     });
   });
 }
+
+export default setPagination;
